120c53cc5554a17f69b7349ea9d3ab81e010f889
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-10-25 14:28:56 -0400

    Add more error checking and logging

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import logging
+import logging.handlers
 import re
 import time
 from concurrent.futures import ThreadPoolExecutor
@@ -8,13 +10,10 @@ import discord
 import pyheif
 import requests
 from discord.ext import commands
-from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 from PIL import Image
-import logging
-import logging.handlers
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -93,193 +92,224 @@ def authenticate_google_drive():
 
 
 def check_folder_exists(folder_name):
-
-    for _ in range(3):
-        try:
-            response = (
-                SERVICE.files()
-                .list(
-                    q=f"'{FOLDER_ID}' in parents and name='{folder_name}'",  # Query to filter by folder parent
-                    corpora="drive",
-                    driveId=SHARED_DRIVE_ID,
-                    includeItemsFromAllDrives=True,
-                    supportsAllDrives=True,
+    try:
+        for _ in range(3):
+            try:
+                response = (
+                    SERVICE.files()
+                    .list(
+                        q=f"'{FOLDER_ID}' in parents and name='{folder_name}'",  # Query to filter by folder parent
+                        corpora="drive",
+                        driveId=SHARED_DRIVE_ID,
+                        includeItemsFromAllDrives=True,
+                        supportsAllDrives=True,
+                    )
+                    .execute()
                 )
-                .execute()
-            )
-            break
-        except Exception as e:
-            logger.debug(f"Failed to find folder: {e}")
-            time.sleep(1)
+                break
+            except Exception as e:
+                logger.debug(f"Failed to find folder: {e}")
+                time.sleep(1)
 
-    folders = response.get("files", [])
-    if folders:
-        return folders[0].get("id")
+        folders = response.get("files", [])
+        if folders:
+            return folders[0].get("id")
 
-    logger.warning(f"Failed to find folder: {folder_name}")
+        logger.debug(f"Failed to find folder: {folder_name}")
+    except Exception as e:
+        logger.error(f"Failed to find folder: {e}")
     return None
 
 
 def create_folder(folder_name):
 
-    # Define the metadata for the new folder
-    folder_metadata = {
-        "name": folder_name,
-        "mimeType": "application/vnd.google-apps.folder",
-        "parents": [FOLDER_ID],  # Set the parent folder in the shared drive
-    }
+    try:
 
-    for _ in range(3):
-        # Create the new folder in the specified shared drive folder
-        try:
-            new_folder = (
-                SERVICE.files()
-                .create(
-                    body=folder_metadata,
-                    supportsAllDrives=True,  # Ensure it supports shared drives
-                    fields="id, name",
+        # Define the metadata for the new folder
+        folder_metadata = {
+            "name": folder_name,
+            "mimeType": "application/vnd.google-apps.folder",
+            "parents": [FOLDER_ID],  # Set the parent folder in the shared drive
+        }
+
+        for _ in range(3):
+            # Create the new folder in the specified shared drive folder
+            try:
+                new_folder = (
+                    SERVICE.files()
+                    .create(
+                        body=folder_metadata,
+                        supportsAllDrives=True,  # Ensure it supports shared drives
+                        fields="id, name",
+                    )
+                    .execute()
                 )
-                .execute()
-            )
-            break
-        except Exception as e:
-            logger.debug(f"Failed to create folder: {e}")
-            time.sleep(3)
+                break
+            except Exception as e:
+                logger.debug(f"Failed to create folder: {e}")
+                time.sleep(3)
 
-    if new_folder:
-        return new_folder.get("id")
+        if new_folder:
+            return new_folder.get("id")
 
-    logger.warning(f"Failed to create folder: {folder_name}")
+        logger.error(f"Failed to create folder: {folder_name}")
+    except Exception as e:
+        logger.error(f"Failed to create folder: {e}")
     return None
 
 
 def upload_image(folder_id, image_data, image_name, extension, thread_name):
 
-    # Define metadata for the new file
-    file_metadata = {
-        "name": image_name,
-        "parents": [folder_id],  # Specify the parent folder ID
-    }
-
-    if extension == "jpg":
-        extension = "jpeg"
-
-    # Set up the media upload using the BytesIO object
     try:
-        media = MediaIoBaseUpload(
-            image_data, mimetype=f"image/{extension}", resumable=True
-        )
-    except Exception as e:
-        logger.debug(f"Failed to create media object: {e}")
+        # Define metadata for the new file
+        file_metadata = {
+            "name": image_name.upper(),
+            "parents": [folder_id],  # Specify the parent folder ID
+        }
 
-    for _ in range(3):
+        if extension == "jpg":
+            extension = "jpeg"
+
+        # Set up the media upload using the BytesIO object
         try:
-            # Upload the file
-            uploaded_image = (
-                SERVICE.files()
-                .create(
-                    body=file_metadata,
-                    media_body=media,
-                    supportsAllDrives=True,  # Ensures compatibility with shared drives
-                    fields="id, name",
-                )
-                .execute()
+            media = MediaIoBaseUpload(
+                image_data, mimetype=f"image/{extension}", resumable=True
             )
-            break
         except Exception as e:
-            logger.debug(f"Failed to upload image: {e}")
-            time.sleep(3)
+            logger.debug(f"Failed to create media object: {e}")
+            return
 
-    if uploaded_image:
-        logger.info(f"{thread_name} {image_name} File ID: {uploaded_image.get("id")}")
-    else:
-        logger.warning(f"Failed to upload image: {image_name}")
-    time.sleep(1)
+        for _ in range(3):
+            try:
+                # Upload the file
+                uploaded_image = (
+                    SERVICE.files()
+                    .create(
+                        body=file_metadata,
+                        media_body=media,
+                        supportsAllDrives=True,  # Ensures compatibility with shared drives
+                        fields="id, name",
+                    )
+                    .execute()
+                )
+                break
+            except Exception as e:
+                logger.debug(f"Failed to upload image: {e}")
+                time.sleep(3)
+
+        if uploaded_image:
+            logger.info(
+                f"Uploaded {image_name} to {thread_name}, File ID: {uploaded_image.get("id")}"
+            )
+        else:
+            logger.warning(f"Failed to upload image: {image_name}")
+        time.sleep(1)
+    except Exception as e:
+        logger.error(f"Failed to upload image: {e}")
 
 
 def download_image(url, file_name, folder_id, extension, thread_name):
-    for _ in range(3):
-        try:
-            response = requests.get(url)
+    try:
+        logger.debug(f"Downloading image from {url}")
+        for _ in range(3):
+            try:
+                response = requests.get(url)
+                logger.debug(f"Response {url}: {response.status_code}")
 
-            if response.status_code == 200:
-                image_data = response.content
-                if "heic" == extension or "heif" == extension:
-                    try:
-                        heif_file = pyheif.read(BytesIO(response.content))
+                if response.status_code == 200:
+                    image_data = response.content
 
-                        # Convert to a Pillow Image object
-                        image = Image.frombytes(
-                            heif_file.mode,
-                            heif_file.size,
-                            heif_file.data,
-                            "raw",
-                            heif_file.mode,
-                            heif_file.stride,
-                        )
+                    logger.debug(f"Downloaded image from {url}")
 
-                        img_bytes = BytesIO()
-                        image.save(img_bytes, format="JPEG")
-                        image_data = img_bytes.getvalue()
-                        extension = "jpeg"
-                        file_name = file_name.replace("heic", "jpeg")
-                    except Exception as e:
-                        logger.debug(f"Failed to convert HEIC/HEIF image: {e}")
-                        image_data = response.content
+                    if "heic" == extension or "heif" == extension:
+                        try:
+                            logger.debug("Converting HEIC/HEIF image")
+                            heif_file = pyheif.read(BytesIO(response.content))
 
-                image_data_bytes = BytesIO(image_data)
+                            # Convert to a Pillow Image object
+                            image = Image.frombytes(
+                                heif_file.mode,
+                                heif_file.size,
+                                heif_file.data,
+                                "raw",
+                                heif_file.mode,
+                                heif_file.stride,
+                            )
 
-                upload_image(
-                    folder_id, image_data_bytes, file_name, extension, thread_name
-                )
-                return
-            else:
-                logger.debug(f"Failed to download image from {url}")
-        except Exception as e:
-            logger.debug(f"Failed to download image: {e}")
-            time.sleep(3)
+                            img_bytes = BytesIO()
+                            image.save(img_bytes, format="JPEG")
+                            image_data = img_bytes.getvalue()
+                            extension = "jpeg"
+                            file_name = file_name.replace("heic", "jpeg")
 
-    logger.warning(f"Failed to download image: {url}")
+                            logger.debug("Converted HEIC/HEIF image")
+                        except Exception as e:
+                            logger.debug(f"Failed to convert HEIC/HEIF image: {e}")
+                            image_data = response.content
+
+                    image_data_bytes = BytesIO(image_data)
+
+                    upload_image(
+                        folder_id, image_data_bytes, file_name, extension, thread_name
+                    )
+                    return
+                else:
+                    logger.debug(f"Failed to download image from {url}")
+            except Exception as e:
+                logger.debug(f"Failed to download image: {e}")
+                time.sleep(3)
+
+        logger.error(f"Failed to download image: {url}")
+    except Exception as e:
+        logger.error(f"Failed to download image: {e}")
 
 
 def queue_image_download(thread_name, attachments, folder_id=None):
 
-    thread_name = thread_name.replace("'", "\\'")
+    try:
+        thread_name = thread_name.replace("'", "\\'")
+        logger.debug(f"Thread Name: {thread_name}")
 
-    if not folder_id:
-        folder_id = check_folder_exists(thread_name)
         if folder_id is None:
-            create_folder(thread_name)
+            folder_id = check_folder_exists(thread_name)
+            if folder_id is None:
+                folder_id = create_folder(thread_name)
 
-    logger.debug(f"FOLDER ID: {folder_id}")
+        logger.info(f"FOLDER ID: {folder_id}")
 
-    for attachment in attachments:
-        url_lower = attachment.url.lower()
-        if any(ext in url_lower for ext in EXTENSIONS):
+        if folder_id:
+            for attachment in attachments:
+                url_lower = attachment.url.lower()
 
-            image_count += 1
+                if any(ext in url_lower for ext in EXTENSIONS):
 
-            file_name = IMAGE_NAME_PATTERN.findall(url_lower)[0]
+                    logger.debug(f"Found image attachment: {attachment.url}")
 
-            file_name = file_name.replace(" ", "_").replace("'", "\\'")
+                    file_name = IMAGE_NAME_PATTERN.findall(url_lower)[0]
 
-            if file_name is None:
-                logger.debug("Could not find image name")
-                continue
+                    file_name = file_name.replace(" ", "_").replace("'", "'")
 
-            # Queue the download task
-            EXECUTOR.submit(
-                download_image,
-                attachment.url,
-                file_name,
-                folder_id,
-                file_name.split(".")[-1],
-                thread_name,
-            )
+                    if file_name is None:
+                        logger.debug("Could not find image name")
+                        continue
+
+                    logger.debug(f"Found image name: {file_name}")
+
+                    # Queue the download task
+                    EXECUTOR.submit(
+                        download_image,
+                        attachment.url,
+                        file_name,
+                        folder_id,
+                        file_name.split(".")[-1],
+                        thread_name,
+                    )
+    except Exception as e:
+        logger.error(f"Failed to queue image download: {e}")
 
 
 async def process_message(message, folder_id=None):
-    if message.attachments:
+    if "no upload" not in message.content.lower() and message.attachments:
         logger.debug(f"Recieved attachments: {message.attachments}")
         thread_name = message.channel.name
         logger.info(f"Recieved message in {thread_name}")
