0c913b0ab65e6d13663125afb0be4b3cb9c46e3f
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-10-25 03:10:49 -0400

    Major improvements that make the bot ready to deploy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
+import asyncio
+import json
 import re
+import time
 from concurrent.futures import ThreadPoolExecutor
 from io import BytesIO
 
@@ -8,10 +10,9 @@ import pyheif
 import requests
 from discord.ext import commands
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaIoBaseUpload
 from PIL import Image
 
 intents = discord.Intents.default()
@@ -19,7 +20,10 @@ intents.message_content = True
 intents.guilds = True
 intents.message_content = True
 
-TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
+CONFIG = {}
+
+SHARED_DRIVE_ID = ""
+FOLDER_ID = ""
 
 EXTENSIONS = (
     ".png",
@@ -28,124 +32,150 @@ EXTENSIONS = (
     ".heic",
     ".heif",
 )
-
-
-SCOPES = ["https://www.googleapis.com/auth/drive"]
-
+SCOPES = [
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/drive.file",
+]
 IMAGE_NAME_PATTERN = re.compile(r"([\w]+\.(?:png|jpg|jpeg|heic|heif))")
-EXTENSION_PATTERN = re.compile(r"\.(?:png|jpg|jpeg|heic|heif)")
 
-service = None
+# google service
+SERVICE = None
 
+# discord commands bot
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Create a thread pool for downloading images
-executor = ThreadPoolExecutor(max_workers=5)
+EXECUTOR = ThreadPoolExecutor(max_workers=1)
 
-# If modifying these SCOPES, delete the file token.json.
+
+def validate_config(config) -> None:
+    """Validate the configuration file to ensure all required keys are present"""
+    if any(
+        key not in config
+        for key in [
+            "DISCORD_TOKEN",
+            "PARENT_FOLDER_ID",
+            "CHANNEL_NAME",
+        ]
+    ):
+        print("Missing required configuration keys")
+        exit(1)
 
 
 def authenticate_google_drive():
-    """Authenticate the user and return a service object."""
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is created automatically
-    # when the authorization flow completes for the first time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-        print("Missing google service credentials")
-        exit(1)
-    # If there are no valid credentials, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+    """Authenticate the user and return a service object"""
+    print("authenticating google cloud service account")
+    creds = Credentials.from_service_account_file(
+        "config/service-credentials.json", scopes=SCOPES
+    )
+    delegated_creds = creds.with_subject("glump@apoez.org")
 
-    service = build("drive", "v3", credentials=creds)
+    print("creating google cloud service")
+    service = build("drive", "v3", credentials=delegated_creds)
     return service
 
 
-def check_folder_exists(service, folder_name, parent_folder_id=None):
-    """Checks if a folder exists in the Google Drive, optionally within a specified parent folder.
+def check_folder_exists(folder_name):
 
-    Args:
-        service: The authenticated Google Drive API service.
-        folder_name: The name of the folder to check.
-        parent_folder_id: The ID of the parent folder (optional).
+    for _ in range(3):
+        try:
+            response = (
+                SERVICE.files()
+                .list(
+                    q=f"'{FOLDER_ID}' in parents and name='{folder_name}'",  # Query to filter by folder parent
+                    corpora="drive",
+                    driveId=SHARED_DRIVE_ID,
+                    includeItemsFromAllDrives=True,
+                    supportsAllDrives=True,
+                )
+                .execute()
+            )
+            break
+        except Exception as e:
+            print(f"Failed to find folder: {e}")
+            time.sleep(1)
 
-    Returns:
-        The folder ID if the folder exists, otherwise None.
-    """
-
-    query = f"mimeType='application/vnd.google-apps.folder' and name='{folder_name}'"
-    if parent_folder_id:
-        query += f" and parents='{parent_folder_id}'"
-
-    results = service.files().list(q=query).execute()
-    items = results.get("files", [])
-
-    if items:
-        return items[0]["id"]
-    else:
-        return None
+    folders = response.get("files", [])
+    if folders:
+        return folders[0].get("id")
+    return None
 
 
-def create_folder(service, folder_name, parent_folder_id=None):
-    """Creates a new folder in the Google Drive, optionally within a specified parent folder.
+def create_folder(folder_name):
 
-    Args:
-        service: The authenticated Google Drive API service.
-        folder_name: The name of the new folder.
-        parent_folder_id: The ID of the parent folder (optional).
-
-    Returns:
-        The ID of the newly created folder.
-    """
-
-    file_metadata = {
+    # Define the metadata for the new folder
+    folder_metadata = {
         "name": folder_name,
         "mimeType": "application/vnd.google-apps.folder",
+        "parents": [FOLDER_ID],  # Set the parent folder in the shared drive
     }
 
-    if parent_folder_id:
-        file_metadata["parents"] = [parent_folder_id]
+    for _ in range(3):
+        # Create the new folder in the specified shared drive folder
+        try:
+            new_folder = (
+                SERVICE.files()
+                .create(
+                    body=folder_metadata,
+                    supportsAllDrives=True,  # Ensure it supports shared drives
+                    fields="id, name",
+                )
+                .execute()
+            )
+            break
+        except Exception as e:
+            print(f"Failed to create folder: {e}")
+            time.sleep(3)
 
-    folder = service.files().create(body=file_metadata, fields="id").execute()
+    if new_folder:
+        return new_folder.get("id")
+    return None
 
-    return folder.get("id")
 
+def upload_image(folder_id, image_data, image_name, extension, thread_name):
 
-def upload_image(service, folder_id, image_data, image_name, extension):
-    """Uploads an image directly from the response content to a specified folder in Google Drive.
-
-    Args:
-        service: The authenticated Google Drive API service.
-        folder_id: The ID of the folder where the image will be uploaded.
-        image_data: The image data as a bytes object.
-        image_name: The desired name for the uploaded image.
-    """
-
-    file_metadata = {"name": image_name, "parents": [folder_id]}
+    # Define metadata for the new file
+    file_metadata = {
+        "name": image_name,
+        "parents": [folder_id],  # Specify the parent folder ID
+    }
 
     if extension == "jpg":
         extension = "jpeg"
 
-    media = {"mimeType": f"image/{extension}", "body": image_data}
+    # Set up the media upload using the BytesIO object
+    try:
+        media = MediaIoBaseUpload(
+            image_data, mimetype=f"image/{extension}", resumable=True
+        )
+    except Exception as e:
+        print(f"Failed to create media object: {e}")
 
-    file = (
-        service.files()
-        .create(body=file_metadata, media_body=media, fields="id")
-        .execute()
-    )
-    print("File ID: {}".format(file.get("id")))
+    for _ in range(3):
+        try:
+            # Upload the file
+            uploaded_image = (
+                SERVICE.files()
+                .create(
+                    body=file_metadata,
+                    media_body=media,
+                    supportsAllDrives=True,  # Ensures compatibility with shared drives
+                    fields="id, name",
+                )
+                .execute()
+            )
+            break
+        except Exception as e:
+            print(f"Failed to upload image: {e}")
+            time.sleep(3)
+
+    print(f"{thread_name} File ID: {uploaded_image.get("id")}")
+    time.sleep(1)
 
 
-def download_image(url, file_name, service, folder_id, extension):
+def download_image(url, file_name, folder_id, extension, thread_name):
     response = requests.get(url)
+
     if response.status_code == 200:
         image_data = response.content
         if "heic" == extension or "heif" == extension:
@@ -171,10 +201,41 @@ def download_image(url, file_name, service, folder_id, extension):
                 print(f"Failed to convert HEIC/HEIF image: {e}")
                 image_data = response.content
 
-        upload_image(service, folder_id, image_data, file_name, extension)
-        print("done")
+        image_data_bytes = BytesIO(image_data)
+
+        upload_image(folder_id, image_data_bytes, file_name, extension, thread_name)
     else:
         print(f"Failed to download image from {url}")
+
+
+def queue_image_download(thread_name, attachments):
+    folder_id = check_folder_exists(thread_name)
+    if folder_id is None:
+        create_folder(thread_name)
+
+    image_count = 0
+
+    for attachment in attachments:
+        url_lower = attachment.url.lower()
+        if any(ext in url_lower for ext in EXTENSIONS):
+
+            image_count += 1
+
+            file_name = IMAGE_NAME_PATTERN.findall(url_lower)[0]
+
+            if file_name is None:
+                print("Could not find image name")
+                continue
+
+            # Queue the download task
+            EXECUTOR.submit(
+                download_image,
+                attachment.url,
+                file_name,
+                folder_id,
+                file_name.split(".")[-1],
+                thread_name,
+            )
 
 
 @bot.event
@@ -184,55 +245,39 @@ async def on_ready() -> None:
 
 @bot.event
 async def on_message(message: discord.message.Message) -> None:
-    print(message)
 
-    if (
-        isinstance(message.channel, discord.Thread)
-        and str(message.channel.parent) == "brother-photos"
+    if isinstance(message.channel, discord.Thread) and CONFIG["CHANNEL_NAME"] == str(
+        message.channel.parent
     ):
-        print("Message is in a thread in brothers photos")
-        print(message.channel.name)
-        if message.attachments:  # Check if the message has attachments
-            for attachment in message.attachments:
-                print("attachment")
-                print(attachment.url)
-                url_lower = attachment.url.lower()
-                if any(ext in url_lower for ext in EXTENSIONS):
-                    # Access the name of the thread
-                    thread_name = message.channel.name
-                    file_name = IMAGE_NAME_PATTERN.findall(url_lower)[0].upper()
-                    print(f"Thread Name: {thread_name}")
-                    print(f"Image URL: {attachment.url}")
-                    print(file_name)
+        if message.attachments:
+            thread_name = message.channel.name
+            print("Recieved message in", thread_name)
 
-                    if file_name is None:
-                        print("Could not find image name")
-                        continue
+            EXECUTOR.submit(queue_image_download, thread_name, message.attachments)
 
-                    folder_id = check_folder_exists(service, thread_name, "parent_id")
-                    if folder_id is None:
-                        create_folder(service, thread_name, "parent_id")
-
-                    # Download the image
-                    executor.submit(
-                        download_image,
-                        attachment.url,
-                        file_name,
-                        service,
-                        folder_id,
-                        EXTENSION_PATTERN.findall(file_name)[0],
-                    )
+            if message.guild is not None:
+                emoji = discord.utils.get(message.guild.emojis, name="glump_photo")
+                if emoji:
+                    await message.add_reaction(emoji)
+                else:
+                    await message.add_reaction("👍")
+            else:
+                await message.add_reaction("👍")
 
 
 if __name__ == "__main__":
-    if TOKEN is None:
-        print("Please set DISCORD_BOT_TOKEN environment variables.")
+    with open("config/config.json", "r") as config_file:
+        CONFIG = json.load(config_file)
+
+    validate_config(CONFIG)
+
+    SHARED_DRIVE_ID = CONFIG["SHARED_DRIVE_ID"]
+    FOLDER_ID = CONFIG["PARENT_FOLDER_ID"]
+
+    SERVICE = authenticate_google_drive()
+
+    if not SERVICE:
+        print("Failed to authenticate Google Drive service")
         exit(1)
 
-    # Ensure the downloads folder exists
-    if not os.path.exists("downloads"):
-        os.makedirs("downloads")
-
-    service = authenticate_google_drive()
-
-    bot.run(TOKEN)
+    bot.run(CONFIG["DISCORD_TOKEN"])
